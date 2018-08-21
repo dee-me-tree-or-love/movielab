5de764b7893307996bb5275a8dc38263b9c66411
dmitrii <dmitry-orlov@hotmail.com>
2018-08-22 00:35:12 +0200

    added hello world resp

--- examples/v1/lookup/app.py
+++ examples/v1/lookup/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/movie/our/<movie_id>")
 def get_movie_from_our_information_api():
-    
+    return "Hello World!"
 
 @app.route("/movie/external/<movie_id>")
 def get_movie_from_external_information_api():
